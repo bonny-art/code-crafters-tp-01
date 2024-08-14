08e7ad09b7016a9e5205e9105a9f8a0402bb9e9a
Andriy <andygrigs88@gmail.com>
2024-08-14 08:38:58 +0200

    created files for adress.py and email.py, made class Email

---
+++ bot/models/email.py
@@ -0,0 +1,30 @@
+import re
+from .field import Field
+
+class Email(Field):
+    """
+    A class representing an email address with validation.
+
+    Inherits:
+        Field: The base class representing a simple data field.
+
+    Attributes:
+        value (str): The email address stored in the field.
+
+    Methods:
+        __init__(value: str) -> None:
+            Initializes a new Email instance with validation.
+    """
+    def __init__(self, value: str) -> None:
+        """
+        Initializes a new Email instance with a given value, ensuring it is a valid email address.
+
+        Args:
+            value (str): The email address to be stored in the field.
+
+        Raises:
+            ValueError: If the email address does not match the pattern.
+        """
+        if not re.fullmatch(r'[a-zA-z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+', value):
+               raise ValueError('Invalid email format')
+        super().__init__(value)
